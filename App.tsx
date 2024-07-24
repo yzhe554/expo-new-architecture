f7d073a598ed0f8a5b08cc80d1208e984de1dc13
Yu Zheng <yu.zheng@cba.com.au>
2024-07-24 11:49:29 +1000

    font scale issue in new arch

--- App.tsx
+++ App.tsx
@@ -3,37 +3,7 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { Image } from 'expo-image';
 import { LinearGradient } from 'expo-linear-gradient';
 
-import './unistyles'
-import { UnistylesRuntime, createStyleSheet, useStyles } from 'react-native-unistyles';
-
-const stylesheet = createStyleSheet((theme, rt) => {
-  console.log(`rt.contentSizeCategory: ${rt.contentSizeCategory}`)
-  console.log(`rt.fontScale: ${rt.fontScale}`)
-  
-  // my styles
-  return {
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-    label: {
-      fontSize: 20,
-    },
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  }
-});
-
 export default function App() {
-  const { styles } = useStyles(stylesheet)
-  // console.log(`UnistylesRuntime.contentSizeCategory: ${UnistylesRuntime.contentSizeCategory}`)
-  // console.log(`UnistylesRuntime.fontScale: ${UnistylesRuntime.fontScale}`)
   return (
     <View style={{
       flex: 1,
@@ -44,12 +14,6 @@ export default function App() {
       <Text style={{fontSize: 24}}>RN 24 Label Testing!!!</Text>
       <Text style={{fontSize: 18}}>RN  18 Label Testing!!!</Text>
       <Text style={{fontSize: 16}}>RN 16 Label Testing!!!</Text>
-       {/* <TextInput
-        style={styles.input}
-        // onChangeText={onChangeText}
-        // value={text}
-      /> */}
-
       < StatusBar style="auto" />
     </View>
   );
